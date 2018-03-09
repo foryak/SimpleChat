088b5cbf1fb09d9d05bd7103e84067c04b1f0cd8
Wadim <wadim.kazak.l@gmail.com>
2018-03-09 15:21:11 +0300

    Initial commit

---
+++ client/src/by/yakovchik/chat/client/ClientWindow.java
@@ -0,0 +1,8 @@
+package by.yakovchik.chat.client;
+
+public class ClientWindow {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ server/src/by/yakovchik/chat/server/ChatServer.java
@@ -0,0 +1,8 @@
+package by.yakovchik.chat.server;
+
+public class ChatServer {
+
+    public static void main(String[] args) {
+        
+    }
+}
